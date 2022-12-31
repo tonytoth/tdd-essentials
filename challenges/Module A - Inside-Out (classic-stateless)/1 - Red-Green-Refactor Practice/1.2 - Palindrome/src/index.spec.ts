060bc8b7abf7fd6080ca8ef9303ac423d6262f36
Tony <tonytoth1337@gmail.com>
2022-12-31 15:03:06 +0200

    RED-GREEN class should exist

--- challenges/Module A - Inside-Out (classic-stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
@@ -1,4 +1,7 @@
+import { PalindromeChecker } from './';
 
 describe('palindrome checker', () => {
-
+    it('class should exist', () => {
+        const palindromCheker = new PalindromeChecker();
+    })
 })
