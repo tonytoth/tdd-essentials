8d42aec6e11dc1d73a0fd3402c104d31080bff9e
Tony <tonytoth1337@gmail.com>
2022-12-31 15:24:18 +0200

    RED-GREEN class should have a method to check if a string is a palindrome

--- challenges/Module A - Inside-Out (classic-stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
@@ -3,5 +3,12 @@ import { PalindromeChecker } from './';
 describe('palindrome checker', () => {
     it('class should exist', () => {
         const palindromCheker = new PalindromeChecker();
+        expect(palindromCheker).toBeDefined();
+    })
+
+    it('should have a method to check if a string is palindrome', () => {
+        const palindromCheker = new PalindromeChecker();
+        console.log('palindromCheker: ', palindromCheker);
+        expect(palindromCheker.checkIfIsAPalindrome).toBeDefined();
     })
 })
