8df0212fd64ab26a84852a772a037ad373dc57f2
Tony <tonytoth1337@gmail.com>
2023-01-02 15:29:15 +0200

    RED-GREEN password validator should have a validation method

--- challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -4,5 +4,10 @@ describe('password validator', () => {
     it('class should exist', () => {
         expect(PasswordValidator).toBeDefined()
     })
+
+    it('should have a validaton method', () => {
+        const passwordValidator = new PasswordValidator();
+        expect(passwordValidator.validate).toBeDefined();
+    })
 })
 
