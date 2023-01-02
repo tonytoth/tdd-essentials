1e8424106d8909d74f25f7e35686a1bf6f45b147
Tony <tonytoth1337@gmail.com>
2023-01-02 15:27:08 +0200

    RED-GREEN password validator should exist

--- challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -1,5 +1,8 @@
+import { PasswordValidator } from './';
 
 describe('password validator', () => {
-
+    it('class should exist', () => {
+        expect(PasswordValidator).toBeDefined()
+    })
 })
 
