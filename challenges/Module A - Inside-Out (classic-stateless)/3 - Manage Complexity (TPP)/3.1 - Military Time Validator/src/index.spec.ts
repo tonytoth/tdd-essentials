a62a0473e56de944a665f208eace97083de1a1e6
Tony <tonytoth1337@gmail.com>
2023-01-05 16:26:55 +0200

    REFACTOR

--- challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
@@ -1,21 +1,21 @@
 import { MilitaryTimeValidator } from './';
 
 describe('military time validator', () => {
-    it('should exist', () => {
-        const militaryTimeValidator = new MilitaryTimeValidator();
+    let militaryTimeValidator: MilitaryTimeValidator;
 
+    beforeAll(() => {
+        militaryTimeValidator = new MilitaryTimeValidator();
+    })
+
+    it('should exist', () => {
         expect(militaryTimeValidator).toBeDefined();
     })
 
     it('should have a validation method', () => {
-        const militaryTimeValidator = new MilitaryTimeValidator();
-
         expect(militaryTimeValidator.validate).toBeDefined();
     })
 
     it('should be able to tell us that "01:12 - 14:32" is a valid military time', () => {
-        const militaryTimeValidator = new MilitaryTimeValidator();
-
         expect(militaryTimeValidator.validate("01:12 - 14:32")).toBeTruthy();
     })
 })
