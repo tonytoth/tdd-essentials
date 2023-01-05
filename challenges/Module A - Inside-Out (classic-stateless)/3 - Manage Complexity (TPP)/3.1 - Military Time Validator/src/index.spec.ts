3ed0e31416ca33ec9f6b3805b7c87c47da479e95
Tony <tonytoth1337@gmail.com>
2023-01-05 16:25:38 +0200

    RED-GREEN should be able to tell us that '01:12 - 14:32' is a valid military time

--- challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
@@ -3,7 +3,7 @@ import { MilitaryTimeValidator } from './';
 describe('military time validator', () => {
     it('should exist', () => {
         const militaryTimeValidator = new MilitaryTimeValidator();
-        
+
         expect(militaryTimeValidator).toBeDefined();
     })
 
@@ -11,5 +11,11 @@ describe('military time validator', () => {
         const militaryTimeValidator = new MilitaryTimeValidator();
 
         expect(militaryTimeValidator.validate).toBeDefined();
+    })
+
+    it('should be able to tell us that "01:12 - 14:32" is a valid military time', () => {
+        const militaryTimeValidator = new MilitaryTimeValidator();
+
+        expect(militaryTimeValidator.validate("01:12 - 14:32")).toBeTruthy();
     })
 })
--- challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.ts
@@ -1,5 +1,5 @@
 export class MilitaryTimeValidator {
-    validate() {
-        
+    validate(time: string) {
+        return true;
     }
 }
