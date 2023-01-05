a4c4b8995201c63dfce4026b5169592ff7e4c8a4
Tony <tonytoth1337@gmail.com>
2023-01-05 10:42:42 +0200

    RED-GREEN should exist a military time validator method

--- challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
@@ -3,6 +3,13 @@ import { MilitaryTimeValidator } from './';
 describe('military time validator', () => {
     it('should exist', () => {
         const militaryTimeValidator = new MilitaryTimeValidator();
+        
+        expect(militaryTimeValidator).toBeDefined();
     })
 
+    it('should have a validation method', () => {
+        const militaryTimeValidator = new MilitaryTimeValidator();
+
+        expect(militaryTimeValidator.validate).toBeDefined();
+    })
 })
